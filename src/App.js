137ca1bf8447ab3c443bac4b351c3c9231eb4339
Putu Abdi Gunawan <user@Abdi-Gunawan-MacBook-Pro.local>
2023-03-14 10:31:52 +0800

    ini adalah pertama commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const App = () => {
+  return (
+    <View>
+      <View style={{ width: 90, height: 90, backgroundColor: '#0Abde3' }} />
+    <Text>Putu</Text>
+    <Text>Abdi</Text>
+    <Text>Gunnnnn</Text>
+  </View>
+  );
+};
+export default App;
